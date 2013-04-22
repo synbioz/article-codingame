cb92314fa5dd2e025370da60023fc322f303c472
Nicolas ZERMATI <nicoolas25@gmail.com>
2013-04-22 16:40:46 +0200

    use objects and rework the algorithm

--- script.rb
+++ script.rb
@@ -27,38 +27,82 @@ $ascii_morse_mapping = {
   'Z' => '--..'
 }
 
-$sequence = gets.chomp
-$dico = (1..gets.to_i).map do
-  result = ''
-  gets.chomp.each_char{ |char| result << $ascii_morse_mapping.fetch(char) }
-  result
+class Word
+  attr_reader :morse
+
+  def initialize(ascii_text)
+    @morse = ''
+    ascii_text.each_char{ |char| @morse << $ascii_morse_mapping.fetch(char) }
+  end
 end
 
-$possibilities_stats = {calls: 0, nodes: 0, cuts: 0, match: 0, unmatch: 0}
-$possibilities_memory = {}
-def possibilities(seq)
-  $possibilities_stats[:calls] += 1
-  return 1 if seq.size == 0
-
-  if $possibilities_memory.has_key?(seq)
-    $possibilities_stats[:cuts] += 1
-    return $possibilities_memory[seq]
+class Dictionnary
+  def initialize
+    @_hash = {}
   end
 
-  $possibilities_stats[:nodes] += 1
+  def push(word)
+    @_max_size = nil
+    @_min_size = nil
+    morse = word.morse
+    @_hash[morse] = count(morse) + 1
+  end
 
-  result = $dico.reduce(0) do |total, word|
-    if word == seq[0, word.size]
-      total += possibilities(seq[word.size, seq.size - word.size])
-      $possibilities_stats[:match] += 1
-    else
-      $possibilities_stats[:unmatch] += 1
+  def count(morse)
+    @_hash[morse] || 0
+  end
+
+  def max_size
+    @_max_size ||= @_hash.keys.max_by(&:size).size
+  end
+
+  def min_size
+    @_min_size ||= @_hash.keys.min_by(&:size).size
+  end
+end
+
+class Problem
+  def initialize(dict, sequence)
+    @dict     = dict
+    @sequence = sequence
+    @_memory  = {}
+  end
+
+  def solve
+    solve_subsequence(@sequence)
+  end
+
+  protected
+
+    def solve_subsequence(seq)
+      return 1 if seq.size == 0
+
+      # Memoize the calls to solve_subsequence
+      return @_memory[seq] if @_memory.has_key?(seq)
+
+      possibilities = 0
+      min = @dict.min_size
+      max = [@dict.max_size, seq.size].min
+      for size in (min..max)
+        count = @dict.count(seq[0, size])
+        if count > 0
+          possibilities += count * solve_subsequence(seq[size, seq.size - size])
+        end
+      end
+
+      @_memory[seq] = possibilities
     end
-    total
-  end
-
-  $possibilities_memory[seq] = result
 end
 
-puts possibilities($sequence)
-STDERR.puts $possibilities_stats.inspect
+sequence = gets.chomp
+dict     = Dictionnary.new
+
+# Fill the dictionnary
+gets.to_i.times { dict.push Word.new(gets.chomp) }
+
+# Solve the problem
+problem  = Problem.new(dict, sequence)
+solution = problem.solve
+
+# Print the solution
+puts solution
